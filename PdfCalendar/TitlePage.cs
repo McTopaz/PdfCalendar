a192cdee5bc189933a1e804f531cda8d19373238
Stefan Eriksson <stefan.k.j.eriksson@gmail.com>
2019-10-10 10:07:56 +0200

    Added a basic title page.

---
+++ PdfCalendar/TitlePage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PdfCalendar
+{
+    class TitlePage : Paragraph
+    {
+        public TitlePage(int year)
+        {
+            var text = $"Kalender {year}";
+            var font = FontHandler.FontFromName("Arial");
+            font.SetStyle("bold");
+            font.Size = 78;
+
+            var paragraph = new Paragraph(text, font)
+            {
+                Alignment = Element.ALIGN_CENTER
+            };
+            paragraph.Chunks.First().SetBackground(BaseColor.WHITE);
+            this.Add(paragraph);
+        }
+    }
+}
