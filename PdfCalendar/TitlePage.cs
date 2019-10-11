2125ef80ac35608c20326ad442c54c1d339d3238
Stefan Eriksson <stefan.k.j.eriksson@gmail.com>
2019-10-11 13:37:57 +0200

    Completely remade the title page. The background is now "more" white than before.

--- PdfCalendar/TitlePage.cs
+++ PdfCalendar/TitlePage.cs
@@ -9,11 +9,63 @@ using iTextSharp.text.pdf;
 
 namespace PdfCalendar
 {
-    class TitlePage : Paragraph
+    class TitlePage : PdfPTable
     {
-        public TitlePage(int year)
+        /*
+        Note:
+         * Since the PDF-document's background color is set to black, it's unknown if it's possible to have different page colors.
+         * By creating the title page as a table and fill the table with many rows, it's somewhat possible to have another background color.
+         * To have another background than white. Change all cell's background to the specific color.
+         * There will be a black border around the table. This is the document's background color.
+        */
+
+        readonly int Year;
+        const int Width = 800;
+
+        public TitlePage(int year) : base(1)
         {
-            var text = $"Kalender {year}";
+            Year = year;
+            Setup();
+            Foo();
+        }
+
+        private void Setup()
+        {
+            HorizontalAlignment = Element.ALIGN_CENTER;
+            TotalWidth = Width;
+            LockedWidth = true;
+        }
+
+        private void Foo()
+        {
+            // White spaces before the title.
+            for(int i = 0; i < 15; i++)
+            {
+                DistanceRow();
+            }
+
+            Title();
+
+            // White spaces after the title.
+            for (int i = 0; i < 15; i++)
+            {
+                DistanceRow();
+            }
+        }
+
+        private void DistanceRow()
+        {
+            var cell = new PdfPCell(new Phrase(Environment.NewLine))
+            {
+                BackgroundColor = BaseColor.WHITE,
+                Border = PdfPCell.NO_BORDER
+            };
+            AddCell(cell);
+        }
+
+        private void Title()
+        {
+            var text = $"Kalender {Year}";
             var font = FontHandler.FontFromName("Arial");
             font.SetStyle("bold");
             font.Size = 78;
@@ -22,8 +74,14 @@ namespace PdfCalendar
             {
                 Alignment = Element.ALIGN_CENTER
             };
-            paragraph.Chunks.First().SetBackground(BaseColor.WHITE);
-            this.Add(paragraph);
+
+            var cell = new PdfPCell(paragraph)
+            {
+                HorizontalAlignment = PdfPCell.ALIGN_CENTER,
+                Border = PdfPCell.NO_BORDER,
+                BackgroundColor = BaseColor.WHITE
+            };
+            AddCell(cell);
         }
     }
 }
