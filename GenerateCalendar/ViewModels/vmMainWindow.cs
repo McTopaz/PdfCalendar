b1b9f11a8fb326921ffb229694c051941ff8fbb7
Stefan Eriksson <stefan.k.j.eriksson@gmail.com>
2019-11-06 15:02:51 +0100

    Adding support for software versions.

---
+++ GenerateCalendar/ViewModels/vmMainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PropertyChanged;
+
+namespace GenerateCalendar.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    class vmMainWindow
+    {
+        public string GuiVersion { get; private set; }
+        public string ApiVerison { get; private set; }
+
+        public vmMainWindow()
+        {
+            GuiVersion = "gui";
+            ApiVerison = "api";
+        }
+    }
+}
