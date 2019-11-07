3f0f9027f5cef695e3ed2e71ba171fd5c1d71c71
Stefan Eriksson <stefan.k.j.eriksson@gmail.com>
2019-11-07 10:26:41 +0100

    Added internaltional women's day.

--- PdfCalendar/Handlers/TeamDayHandler.cs
+++ PdfCalendar/Handlers/TeamDayHandler.cs
@@ -30,6 +30,7 @@ namespace PdfCalendar.Handlers
             var brailleDay = new DateTime(Year, 1, 4);
             var heart = new DateTime(Year, 2, 14);
             var swedenFinns = new DateTime(Year, 2, 24);
+            var womansDay = new DateTime(Year, 3, 8);
             var cinnamonBun = new DateTime(Year, 10, 4);
             var unDay = new DateTime(Year, 10, 24);         // United Nation's day.
             var kladdkaka = new DateTime(Year, 11, 7);
@@ -41,8 +42,9 @@ namespace PdfCalendar.Handlers
             var list = new List<(DateTime, Bitmap, float, float, string)>();
             list.Add((brailleDay, Images.Braille, 16, 16, "Punktskriftsdagen"));
             list.Add((brailleDay, Images.Eye, 12, 12, "Världshypnosdagen"));        // Same day as Braille day.
-            list.Add((swedenFinns, Images.SwedenFinnsFlag, 15, 10, "Sverigefinnarnas dag"));
             list.Add((heart, Images.Heart, 13, 13, "Alla hjärtans dag"));
+            list.Add((swedenFinns, Images.SwedenFinnsFlag, 15, 10, "Sverigefinnarnas dag"));
+            list.Add((womansDay, Images.Woman, 16, 16, "Internationella kvinnodagen"));
             list.Add((mothersDay, Images.Woman, 16, 16,"Mors dag"));
             list.Add((unDay, Images.UNFlag, 16, 10, "FN-dagen"));
             list.Add((cinnamonBun, Images.CinnamonBun, 16, 16, "Kanelbullens dag"));
