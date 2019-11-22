797d33ac4905a1e7c686127a2a8a6f3d5dac4023
Stefan Eriksson <stefan.k.j.eriksson@gmail.com>
2019-11-22 14:47:30 +0100

    Removed the Test project.

--- Test/Program.cs
+++ Test/Program.cs
@@ -11,17 +11,31 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            var d0 = new DateTime(2019, 10, 20);
+            var d1 = new DateTime(2018, 10, 20);
+            var d2 = new DateTime(2019, 10, 21);
+            var d3 = new DateTime(2019, 10, 20);
+            var d4 = new DateTime(2017, 10, 21);
+            var d5 = new DateTime(2019, 10, 22);
+            var d6 = new DateTime(2015, 10, 21);
+            var d7 = new DateTime(2010, 10, 23);
+            var d8 = new DateTime(1995, 10, 20);
+            var d9 = new DateTime(2019, 10, 22);
+
             var list = new List<(DateTime Date, string Name)>();
-            var d1 = new DateTime(2019, 10, 20);
-            var d2 = new DateTime(2019, 10, 20);
-            var d3 = new DateTime(2019, 10, 21);
-            var d4 = new DateTime(2019, 10, 20);
+            list.Add((d0, "d0"));
             list.Add((d1, "d1"));
             list.Add((d2, "d2"));
             list.Add((d3, "d3"));
             list.Add((d4, "d4"));
+            list.Add((d5, "d5"));
+            list.Add((d6, "d6"));
+            list.Add((d7, "d7"));
+            list.Add((d8, "d8"));
+            list.Add((d9, "d9"));
 
-            list.Sort();
+
+            var l = list.OrderBy(p => p.Date.Month).ThenBy(p => p.Date.Day);
         }
     }
 }
