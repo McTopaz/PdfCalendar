a33734c95446f014e90b792fc640853ca81e9dc2
Stefan Eriksson <stefan.k.j.eriksson@gmail.com>
2019-11-26 23:55:25 +0100

    Added begioning of GUI for altering page spacing.

---
+++ GenerateCalendar/Data/PageSpacing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using PropertyChanged;
+
+namespace GenerateCalendar.Data
+{
+    [AddINotifyPropertyChangedInterface]
+    class PageSpacing
+    {
+        public int Month { get; set; }
+        public bool Auto { get; set; }
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public int Right { get; set; }
+        public int Bottom { get; set; }
+
+        public PageSpacing()
+        {
+            Auto = true;
+            Left = 0;
+            Top = 0;
+            Right = 0;
+            Bottom = 0;
+        }
+    }
+}
---
+++ GenerateCalendar/ViewModels/vmPageSpacing.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PropertyChanged;
+using GenerateCalendar.Data;
+using GenerateCalendar.Misc;
+
+namespace GenerateCalendar.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    class vmPageSpacing
+    {
+        public ObservableCollection<PageSpacing> PageSpacings { get; set; }
+
+        public vmPageSpacing()
+        {
+            DefaultItems();
+        }
+
+        private void DefaultItems()
+        {
+            var jan = new PageSpacing() { Month = 1, Top = 100 };
+            var feb = new PageSpacing() { Month = 2 };
+            var mar = new PageSpacing() { Month = 3 };
+            var apr = new PageSpacing() { Month = 4 };
+            var may = new PageSpacing() { Month = 5 };
+            var jun = new PageSpacing() { Month = 6 };
+            var jul = new PageSpacing() { Month = 7 };
+            var aug = new PageSpacing() { Month = 8 };
+            var sep = new PageSpacing() { Month = 9 };
+            var oct = new PageSpacing() { Month = 10 };
+            var nov = new PageSpacing() { Month = 11 };
+            var dec = new PageSpacing() { Month = 12 };
+
+            PageSpacings.Add(jan);
+            PageSpacings.Add(feb);
+            PageSpacings.Add(mar);
+            PageSpacings.Add(apr);
+            PageSpacings.Add(may);
+            PageSpacings.Add(jun);
+            PageSpacings.Add(jul);
+            PageSpacings.Add(aug);
+            PageSpacings.Add(sep);
+            PageSpacings.Add(oct);
+            PageSpacings.Add(nov);
+            PageSpacings.Add(dec);
+        }
+    }
+}
