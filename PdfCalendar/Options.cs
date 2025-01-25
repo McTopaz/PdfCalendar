07357ed9595c551f7144927909a8d5aabe603ba1
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2025-01-25 01:49:03 +0100

    Added flags to hide any month.

--- PdfCalendar/Options.cs
+++ PdfCalendar/Options.cs
@@ -14,6 +14,19 @@ namespace PdfCalendar
         public bool DaylightSavingTime { get; set; }    // Summer time.
         public bool StandardTime { get; set; }          // Winter time.
 
+        public bool HideJanuary { get; set; } = false;
+        public bool HideFebruary { get; set; } = false;
+        public bool HideMars { get; set; } = false;
+        public bool HideApril { get; set; } = false;
+        public bool HideMay { get; set; } = false;
+        public bool HideJune { get; set; } = false;
+        public bool HideJuly { get; set; } = false;
+        public bool HideAugust { get; set; } = false;
+        public bool HideSeptember { get; set; } = false;
+        public bool HideOctober { get; set; } = false;
+        public bool HideNovember { get; set; } = false;
+        public bool HideDecember { get; set; } = false;
+
         public Options()
         {
             DaylightSavingTime = true;
